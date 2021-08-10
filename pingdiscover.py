3bd70396d8ddea77c2145647e2e9eb8a5c43afed
Suman Manandhar <smanandhar@encorenetworks.com>
2021-08-10 09:08:58 -0400

    Added comments in the code

--- pingdiscover.py
+++ pingdiscover.py
@@ -13,13 +13,18 @@ async def main:
        parser.add_argument("--concurrent", type=int, help="Number of concurrent pings")
        parser.add_argument("--timeout", type=int, help="Ping timeout value in seconds")
        args = parser.parse_args()
+       #Get list of all possible IP addresses within the network
        addrlist = list(ip_network(arg.ipaddr))
+       #Default concurrent task = 1
        concurrent = 1
+       #Default timeout is 5 seconds
        timeout = 5
-       if args.concurrent >= 1:
+       if args.concurrent > 1:
             concurrent = args.concurrent
+       #Allow timeout down to 1 second
        if args.timeout >= 1:
             timeout = args.timeout
+       #Get the number of available io addresses to ping
        numip = len(addrlist)
        i = 0
 
